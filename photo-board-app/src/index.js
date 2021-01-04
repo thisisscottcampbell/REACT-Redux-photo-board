cdab9739143231c63dc79e04cec614c2d0eac279
Scott Campbell <stc9486@gmail.com>
2021-01-04 17:40:03 -0500

    redux ready

--- photo-board-app/src/index.js
+++ photo-board-app/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles/stylesheet.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
 	<BrowserRouter>
---
+++ photo-board-app/src/redux/reducer.js
@@ -0,0 +1,8 @@
+import dummyPosts from '../util/posts';
+
+const postReducer = function posts(state = dummyPosts, action) {
+	//remove photo action
+	return state;
+};
+
+export default postReducer;
